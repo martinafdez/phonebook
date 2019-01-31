81b7bb212ae5c69ce7895251a5fe1fd198e59c88
Fabiana Lassandro <fabianalassandro@gmail.com>
2019-01-31 15:01:04 +0000

    phonebook_functions.py search for business name

--- phonebook_functions.py
+++ phonebook_functions.py
@@ -60,19 +60,14 @@ def typePostcodeOrCity():
 def typeBizTypeOrName(userInputPostcodeOrCity):
     userInputBizTypeOrName = input("Type in name or type of business: ").title()
 
-    c.execute('SELECT * FROM business WHERE postcode = ? AND typeBusiness OR nameBusiness  = ?', (userInputPostcodeOrCity, userInputBizTypeOrName) )
+    c.execute('SELECT * FROM business WHERE postcode = ? AND typeBusiness = ? OR nameBusiness  = ?', (userInputPostcodeOrCity, userInputBizTypeOrName, userInputBizTypeOrName) )
     resultsFinalPc =  c.fetchall()
     if len(resultsFinalPc) != 0:
             print(resultsFinalPc)
+
     else:
-        c.execute('SELECT * FROM business WHERE city = ? AND typeBusiness OR nameBusiness = ?', (userInputPostcodeOrCity, userInputBizTypeOrName) )
-        resultsFinalC =  c.fetchall()
-        
-        if len(resultsFinalC) != 0:
-            print(resultsFinalC)
-        else:
-            print("Sorry, nothing for " + userInputBizTypeOrName + " in " + userInputPostcodeOrCity + ". Try again!")
-            typePostcodeOrCity()
+        print("Sorry, nothing for " + userInputBizTypeOrName + " in " + userInputPostcodeOrCity + ". Try again!")
+        typePostcodeOrCity()
 
 typePostcodeOrCity()
 
